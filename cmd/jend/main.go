aa69b4d6cf22a003cba46da9365c720c7485f6b9
Anish Jami <jamianish8@gmail.com>
2025-12-17 21:43:39 -0500

    started a basic template for send and recieve commands

---
+++ cmd/jend/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "jend",
+	Short: "JEND: A High-speed, secure P2P file transfer tool",
+	Long:  `A CLI tool for sending files directly between peers using QUIC and PAKE encryption.`,
+}
+
+func main() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// We will add subcommands (send/receive) here in the next step
+	rootCmd.AddCommand(sendCmd)
+	rootCmd.AddCommand(receiveCmd)
+}
+
+var sendCmd = &cobra.Command{
+	Use:   "send [file]",
+	Short: "Send a file or folder",
+	Args:  cobra.MinimumNArgs(1), // Ensures the user provides a file path
+	Run: func(cmd *cobra.Command, args []string) {
+		filePath := args[0]
+		fmt.Printf("Initiating transfer for: %s\n", filePath)
+	},
+}
+
+var receiveCmd = &cobra.Command{
+	Use:   "receive [code]",
+	Short: "Receive a file using a PAKE code",
+	Args:  cobra.ExactArgs(1), // Ensures the user provides the code
+	Run: func(cmd *cobra.Command, args []string) {
+		code := args[0]
+		fmt.Printf("Attempting to receive with code: %s\n", code)
+	},
+}
